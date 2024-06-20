4e075992c241d414c9f8897ad9c2276e3d44c367
Juan Parera <1420988+jparera@users.noreply.github.com>
2024-06-20 20:20:41 +0200

    Implement a simple echo service with gRPC

--- client/src/main/java/net/wrlt/grpc/template/client/Main.java
+++ client/src/main/java/net/wrlt/grpc/template/client/Main.java
@@ -1,10 +1,29 @@
 package net.wrlt.grpc.template.client;
 
-import net.wrlt.grpc.template.shared.proto.Message;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.StatusRuntimeException;
+
+import net.wrlt.grpc.template.shared.proto.EchoGrpc;
+import net.wrlt.grpc.template.shared.proto.EchoRequest;
 
 public class Main {
-    public static void main(String[] args) {
-        var message = Message.newBuilder().setContent("Text").build();
-        System.out.printf("[client] Message = {\n%s}\n", message);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        var channel = Grpc.newChannelBuilder("localhost:50051", InsecureChannelCredentials.create()).build();
+        try {
+            var stub = EchoGrpc.newBlockingStub(channel);
+            try {
+                var request = EchoRequest.newBuilder().setContent("Hello!").build();
+                var reply = stub.echo(request);
+                System.out.println(reply.getContent());
+            } catch(StatusRuntimeException e) {
+                e.printStackTrace();
+            }
+        } finally {
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
     }
 }
--- server/src/main/java/net/wrlt/grpc/template/server/Main.java
+++ server/src/main/java/net/wrlt/grpc/template/server/Main.java
@@ -1,10 +1,32 @@
 package net.wrlt.grpc.template.server;
 
-import net.wrlt.grpc.template.shared.proto.Message;
+import java.io.IOException;
+
+import io.grpc.Grpc;
+import io.grpc.InsecureServerCredentials;
+import io.grpc.stub.StreamObserver;
+
+import net.wrlt.grpc.template.shared.proto.EchoGrpc;
+import net.wrlt.grpc.template.shared.proto.EchoRequest;
+import net.wrlt.grpc.template.shared.proto.EchoReply;
 
 public class Main {
-    public static void main(String[] args) {
-        var message = Message.newBuilder().setContent("Text").build();
-        System.out.printf("[server] Message = {\n%s}\n", message);
+    private static final int PORT = 50051;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        var builder = Grpc.newServerBuilderForPort(PORT, InsecureServerCredentials.create());
+        var server = builder.addService(new EchoService()).build();
+        server.start();
+        System.out.printf("Echo service listening on port %s...\n", PORT);
+        server.awaitTermination();
+    }
+
+    private static class EchoService extends EchoGrpc.EchoImplBase {
+        @Override
+        public void echo(EchoRequest request, StreamObserver<EchoReply> responseObserver) {
+            var reply = EchoReply.newBuilder().setContent(request.getContent()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
     }
 }
